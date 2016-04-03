1370c3f568cbd8a38444187297cd874f4673fcb5
Tyler Long <tyler.stephen.long@gmail.com>
2016-04-03 10:00:26 -0600

    First test

--- circles/circles_test.exs
+++ circles/circles_test.exs
@@ -2,4 +2,8 @@ ExUnit.start
 
 defmodule CirclesTest do
   ExUnit.configure
+  
+  test "it finds two points through shared circle" do 
+    assert Circles.calculate({p1, p2} {p1, p2}, radius) === ({1,2}, {2,3}, 5)
+  end
 end
